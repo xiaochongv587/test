78571bd8147bf8d1adb2435ae6885159e226e57a
xiaochongv587 <1441652193@qq.com>
2020-11-24 17:47:39 +0800

    test demo

---
+++ README.md
@@ -0,0 +1,7 @@
+#### 简单的测试demo
+```
+$obj = new Index()
+```
+#### 打印输出
+
+![avatar](https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1657471424,2319316184&amp;fm=26&amp;gp=0.jpg)
---
+++ src/Index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Creator by Administrator
+ * Author: xiaochong
+ * Date: 2020/11/24
+ * Time: 17:19
+ */
+namespace Lcc\Test;
+
+class Index
+{
+    public function index(){
+        echo "测试一下";
+    }
+}
